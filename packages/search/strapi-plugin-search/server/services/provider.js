2cd6f8f5d921c035d683544cb53fb99a06db5389
Mattias van den Belt <mattiasvandenbelt@gmail.com>
2021-10-02 00:52:56 +0200

    [fix] Example package and required scoped provider

--- packages/search/strapi-plugin-search/server/services/provider.js
+++ packages/search/strapi-plugin-search/server/services/provider.js
@@ -24,7 +24,7 @@ module.exports = () => ({
       // Todo implement v4 package loader logic
       const providerInstance = pluginConfig.instance
         ? pluginConfig.instance
-        : await require(`strapi-provider-search-${pluginConfig.provider}`).init(pluginConfig);
+        : await require(`@mattie-bundle/strapi-provider-search-${pluginConfig.provider}`).init(pluginConfig);
 
       if (validateProvider(providerInstance)) {
         PROVIDER_METHODS.forEach((method) => {
