7ef45ba176fbe77e94f002e6dfd6cba9503ee360
Mattias van den Belt <mattiasvandenbelt@gmail.com>
2022-02-02 00:05:38 +0100

    [chore] Update dependencies and fix peer dependencies (#59)
    * Fix peer dependencies
    
    * Update and migrate to latest Strapi dependencies
    
    * Update yarn.lock
    
    Fixes #52

--- example/config/server.js
+++ example/config/server.js
@@ -3,4 +3,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS', ['3BVKhHFjP4HFcuT6F', '8kifS2raspbuhJ9HV']),
+  },
 });
